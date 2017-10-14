1edbb4c335dce7405cfb8ff97cec0642a1bf2ccd
julien <julien.boyer49@orange.fr>
2017-10-14 17:52:50 +0200

    Fix | Remove useless spaces, fix API url call

--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,7 @@ const v01 = express.Router();
 v01.use((req, res, next) => {
     // Some MiddleWare logic there
 	next();
-});    
+});
 
 v01.get('/', (req, res) => {
     // Render some JsonStuff
