cb1b8e0056f4bd9f95bf542c1a6a60c93bf1c0f4
julien <julien.boyer49@orange.fr>
2017-10-17 22:34:26 +0200

    Material-ui | Wrap all the app in material component

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,21 @@ import "../stylesheets/main.scss";
 import Header from './layouts/Header';
 import Footer from './layouts/Footer';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import lightTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+
 class App extends React.Component {
 
   render() {
     return (
-      <div id="page-wrapper" className="main-app">
-        <Header />
-          {this.props.children}
-        <Footer />
-      </div>
+      <MuiThemeProvider muiTheme={getMuiTheme(lightTheme)}>
+        <div id="page-wrapper" className="main-app">
+          <Header />
+            {this.props.children}
+          <Footer />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppBar from 'material-ui/AppBar';
-import lightTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 
 class Header extends Component {
 
   render() {
     return (
       <header className="header">
-        <MuiThemeProvider muiTheme={getMuiTheme(lightTheme)}>
           <AppBar title="Planning scheduler" />
-        </MuiThemeProvider>
       </header>
     );
   }
