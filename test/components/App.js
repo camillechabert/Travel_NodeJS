6a47b30e36c04dad2d2a80b0b892550f7f37fec8
julien <julien.boyer49@orange.fr>
2017-10-31 18:02:31 +0100

    Update test for new features

--- test/components/App.js
+++ test/components/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { shallow } from "enzyme";
 import assert from "assert";
-import  App  from "../../src/components/App";
+import App from "../../src/components/App";
 
 // unit tests for the App component
 describe('App component', () => {
   describe('render()', () => {
     it('should render the component', () => {
-      const props = {dispatch: ()=>{}, users: []};
-      const wrapper = shallow(<App {...props}/>);
+      const childProps =  { dataPropagation: { userSession: false } };
+      const wrapper = shallow(<App children={childProps} />);
       assert.equal(wrapper.length, 1);
     });
   });
