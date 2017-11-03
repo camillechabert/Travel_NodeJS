92a97c2277cb80f51cc731965112b5360a9b56bc
francois.poisblaud@gmail.com <francois.poisblaud@gmail.com>
2017-11-03 11:54:08 +0100

    New design Edition and Register

--- src/components/Authentication/Edition.js
+++ src/components/Authentication/Edition.js
@@ -1,10 +1,78 @@
 import React, { Component } from "react";
+import { Button, Form, Container, Header, Segment, Label, Image, Card, Grid, Icon } from 'semantic-ui-react'
+
+const extra = (
+    <a>
+      <Icon name='mail' />
+      John.doe@gmail.com
+    </a>
+  )
 
 class Edition extends Component {
     
     render() {
         return (
-            <h1> Edition </h1>
+            <Grid columns={2} divided>
+                <Grid.Row>
+                    <Grid.Column>
+                        <Card
+                        columns={3}
+                        image='/media/logo.svg'
+                        header='John Doe'
+                        meta='inscrit le 10/03/2017'
+                        extra={extra}
+                        fluid
+                        />  
+                    </Grid.Column>
+                    <Grid.Column>
+                            <Form  size='large'>
+
+                                <Form.Group unstackable widths={2}>
+                                    <Form.Input 
+                                    value="John"
+                                    icon='user' 
+                                    iconPosition='left' 
+                                    label='First name' 
+                                    placeholder='First name' />
+                                    <Form.Input 
+                                    value="Doe"
+                                    icon='user' 
+                                    iconPosition='left' 
+                                    label='Last name' 
+                                    placeholder='Last name' />
+                                </Form.Group>
+
+                                <Form.Group widths={2}>
+                                    <Form.Input 
+                                    value="John.doe@gmail.com"
+                                    icon='mail' 
+                                    iconPosition='left' 
+                                    label='E-mail' 
+                                    placeholder='e-mail' 
+                                    />
+                                    <Form.Input 
+                                    icon='unlock alternate' 
+                                    iconPosition='left' 
+                                    type="password" 
+                                    label='Password' 
+                                    placeholder='Password' 
+                                    />
+                                </Form.Group>
+
+                                    <Form.Input 
+                                    className="Token"
+                                    style={{ display: 'none' }}
+                                    />
+                                    <Button 
+                                    color='teal' 
+                                    fluid size='large'
+                                    type='submit'>
+                                    Modify
+                                    </Button>                   
+                                </Form>
+                         </Grid.Column> 
+                    </Grid.Row>
+                </Grid>      
         );
     }
 }
--- src/components/Authentication/Register.js
+++ src/components/Authentication/Register.js
@@ -1,10 +1,64 @@
 import React, { Component } from "react";
+import { Button, Form, Container, Header, Segment, Label, Image } from 'semantic-ui-react'
+
 
 class Register extends Component {
     
     render() {
         return (
-            <h1> Register </h1>
+            <Container>
+                    <Header as='h2' color='teal' textAlign='center'>
+                    <Image src='/media/logo.svg' />
+                    REGISTER
+                    </Header>
+                    <Segment stacked>
+                    <Form  size='large'>
+
+                        <Form.Group unstackable widths={2}>
+                            <Form.Input 
+                            icon='user' 
+                            iconPosition='left' 
+                            label='First name' 
+                            placeholder='First name' />
+                            <Form.Input 
+                            icon='user' 
+                            iconPosition='left' 
+                            label='Last name' 
+                            placeholder='Last name' />
+                        </Form.Group>
+
+                        <Form.Group widths={2}>
+                            <Form.Input 
+                            icon='mail' 
+                            iconPosition='left' 
+                            label='E-mail' 
+                            placeholder='e-mail' 
+                            />
+                            <Form.Input 
+                            icon='unlock alternate' 
+                            iconPosition='left' 
+                            type="password" 
+                            label='Password' 
+                            placeholder='Password' 
+                            />
+                        </Form.Group>
+
+                            <Form.Checkbox 
+                            label='I agree to the Terms and Conditions' 
+                            />
+                            <Form.Input 
+                            className="Token"
+                            style={{ display: 'none' }}
+                            />
+                            <Button 
+                            color='teal' 
+                            fluid size='large'
+                            type='submit'>
+                            Submit
+                            </Button>                   
+                    </Form>
+                </Segment>    
+            </Container>
         );
     }
 }
