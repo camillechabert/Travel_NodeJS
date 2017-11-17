d48b2a7e9a056346821df83d557a1cda65743571
francois.poisblaud@gmail.com <francois.poisblaud@gmail.com>
2017-11-17 15:19:57 +0100

    Fix design edition juh/travel-nodejs#24

--- src/components/Authentication/Edition.js
+++ src/components/Authentication/Edition.js
@@ -12,67 +12,79 @@ class Edition extends Component {
     
     render() {
         return (
-            <Grid columns={2} divided>
-                <Grid.Row>
-                    <Grid.Column>
-                        <Card
-                        columns={3}
-                        image='/media/logo.svg'
-                        header='John Doe'
-                        meta='inscrit le 10/03/2017'
-                        extra={extra}
-                        fluid
-                        />  
-                    </Grid.Column>
-                    <Grid.Column>
-                            <Form  size='large'>
+            <Grid
+            textAlign='center'
+            style={{ height: '50%' }}
+            verticalAlign='middle'
+            >     
+            <Grid.Column>
+            <Header as='h2' color='teal' textAlign='center'>
+            <Image src='/media/logo.svg' />
+            Register your account
+            </Header>
+                    <Grid columns={2} divided>
+                        <Grid.Row>
+                            <Grid.Column>
+                                <Card
+                                columns={3}
+                                image='/media/logo.svg'
+                                header='John Doe'
+                                meta='inscrit le 10/03/2017'
+                                extra={extra}
+                                fluid
+                                />  
+                            </Grid.Column>
+                            <Grid.Column>
+                                    <Form  size='large'>
 
-                                <Form.Group unstackable widths={2}>
-                                    <Form.Input 
-                                    value="John"
-                                    icon='user' 
-                                    iconPosition='left' 
-                                    label='First name' 
-                                    placeholder='First name' />
-                                    <Form.Input 
-                                    value="Doe"
-                                    icon='user' 
-                                    iconPosition='left' 
-                                    label='Last name' 
-                                    placeholder='Last name' />
-                                </Form.Group>
+                                        <Form.Group unstackable widths={2}>
+                                            <Form.Input 
+                                            value="John"
+                                            icon='user' 
+                                            iconPosition='left' 
+                                            label='First name' 
+                                            placeholder='First name' />
+                                            <Form.Input 
+                                            value="Doe"
+                                            icon='user' 
+                                            iconPosition='left' 
+                                            label='Last name' 
+                                            placeholder='Last name' />
+                                        </Form.Group>
 
-                                <Form.Group widths={2}>
-                                    <Form.Input 
-                                    value="John.doe@gmail.com"
-                                    icon='mail' 
-                                    iconPosition='left' 
-                                    label='E-mail' 
-                                    placeholder='e-mail' 
-                                    />
-                                    <Form.Input 
-                                    icon='unlock alternate' 
-                                    iconPosition='left' 
-                                    type="password" 
-                                    label='Password' 
-                                    placeholder='Password' 
-                                    />
-                                </Form.Group>
+                                        <Form.Group widths={2}>
+                                            <Form.Input 
+                                            value="John.doe@gmail.com"
+                                            icon='mail' 
+                                            iconPosition='left' 
+                                            label='E-mail' 
+                                            placeholder='e-mail' 
+                                            />
+                                            <Form.Input 
+                                            icon='unlock alternate' 
+                                            iconPosition='left' 
+                                            type="password" 
+                                            label='Password' 
+                                            placeholder='Password' 
+                                            />
+                                        </Form.Group>
 
-                                    <Form.Input 
-                                    className="Token"
-                                    style={{ display: 'none' }}
-                                    />
-                                    <Button 
-                                    color='teal' 
-                                    fluid size='large'
-                                    type='submit'>
-                                    Modify
-                                    </Button>                   
-                                </Form>
-                         </Grid.Column> 
-                    </Grid.Row>
-                </Grid>      
+                                            <Form.Input 
+                                            className="Token"
+                                            style={{ display: 'none' }}
+                                            />
+                                            <Button 
+                                            color='teal' 
+                                            fluid size='large'
+                                            type='submit'>
+                                            Modify
+                                            </Button>                   
+                                        </Form>
+                                </Grid.Column> 
+                            </Grid.Row>
+                        </Grid>  
+                </Grid.Column>
+            </Grid>               
         );
     }
 }
