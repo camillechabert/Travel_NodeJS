c12ef361244f13562ee66644a3273bf3f596e57e
Greencame <julien.mustiere@ntymail.com>
2017-12-01 17:31:59 +0100

    Camille | Cherry-picked commit, Icons Design

--- src/components/Map/PopupContent.js
+++ src/components/Map/PopupContent.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-import { Icon, Card, Button, Image } from 'semantic-ui-react';
-import PropTypes from 'prop-types';
+import { Card, Button, Image } from 'semantic-ui-react';
 import { addDestCoordinates } from '../../actions/destinationActions';
-import {store} from '../../store';
+import { store } from '../../store';
+import PropTypes from 'prop-types';
+import '../../stylesheets/components/map.scss';
 
 class PopupContent extends Component {
-  addDestination(e) {
+  constructor(props) {
+    super(props);
+
+    this.typeMarkers = {
+      restaurant: '../../../media/icons/cutlery.svg',
+      bar: '../../../media/icons/cocktail.svg',
+      pub: '../../../media/icons/beer.svg',
+      hotel: '../../../media/icons/bed.svg',
+      cafe: '../../../media/icons/coffee-cup.svg'
+    };
+  }
+
+  addDestination() {
     const coords = this.props.POI.lon + ',' + this.props.POI.lat;
     store.dispatch(addDestCoordinates(coords));
   }
@@ -14,7 +27,8 @@ class PopupContent extends Component {
     return (
       <Card>
         <Card.Content>
-          <Image floated='right' size='mini' src={this.props.POI.icon} />
+          <Button className="close" icon='close' onClick={this.props.close} floated="right" />
+          <Image floated='left' size='mini' src={this.typeMarkers[this.props.POI.type]} />
           <Card.Header>
             {this.props.POI.display_name[0]}
           </Card.Header>
@@ -29,9 +43,9 @@ class PopupContent extends Component {
           </Card.Description>
         </Card.Content>
         <Card.Content extra>
-          <div className='ui two buttons'>
-            <Button basic color='green' onClick={(e) => this.addDestination(e)}>Add</Button>
-            <Button onClick={this.props.close} basic color='red'>Close</Button>
+          <div>
+            <Button basic color='green' icon='plus' content='Ajouter' onClick={() => this.addDestination()} />
+            <Button basic color='blue' icon='info' content='Informations' />
           </div>
         </Card.Content>
       </Card>
