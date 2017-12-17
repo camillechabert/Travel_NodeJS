d1659d911790d2593ea3d88600c9cdebc2e8204f
Greencame <julien.mustiere@ntymail.com>
2017-12-17 16:40:20 +0100

    Error optimisation

--- App/errors/index.js
+++ App/errors/index.js
@@ -2,6 +2,6 @@ const BadRequest = require('./badRequest');
 const NotFound = require('./notFound');
 
 module.exports = {
-  BadRequest: BadRequest,
-  NotFound: NotFound
+  BadRequest,
+  NotFound
 };
